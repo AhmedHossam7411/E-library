538d16d9571357cce260560a2a8fd8fa9aeab74e
Ahany42 <ahanyfathy42@gmail.com>
2023-09-14 22:03:58 +0300

    books EventListener

--- read.js
+++ read.js
@@ -12,7 +12,25 @@ var filterresponse=document.getElementById("filterresponse");
 var menuicon = document.getElementById("menubaricon");
 var menucontainer = document.getElementById("menubarcontainer");
 var readsection = document.getElementById("readsection");
-
+var AZbooks =  document.getElementById("AZbooks");
+var ZAbooks =  document.getElementById("ZAbooks");
+var A1 =  document.getElementById("A1");
+var A2 =  document.getElementById("A2");
+var A3 =  document.getElementById("A3");
+var A4 =  document.getElementById("A4");
+var B1 =  document.getElementById("B1");
+var B2 =  document.getElementById("B2");
+var B3 =  document.getElementById("B3");
+var B4 =  document.getElementById("B4");
+var ZA1 =  document.getElementById("ZA1");
+var ZA2 =  document.getElementById("ZA2");
+var ZA3 =  document.getElementById("ZA3");
+var ZA4 =  document.getElementById("ZA4");
+var ZB1 =  document.getElementById("ZB1");
+var ZB2 =  document.getElementById("ZB2");
+var ZB3 =  document.getElementById("ZB3");
+var ZB4 =  document.getElementById("ZB4");
+var isAz=true;
 
 fnDate();
 function fnDate(){
@@ -43,12 +61,21 @@ function apply(){
     AZ.style.color="#fff";
     ZA.style.backgroundColor="black";
     ZA.style.color="#fff";
+    if(isAz==false){
+        ZAbooks.style.display="block";
+        AZbooks.style.display="none";  
+    }
+    else{
+        AZbooks.style.display="block";
+        ZAbooks.style.display="none";
+    }
 }
 function AZfun(){
 AZ.style.backgroundColor="#1b3a2f";
 AZ.style.color="white";
 ZA.style.backgroundColor="black";
 ZA.style.color="white";
+isAz=true;
 
 }
 function ZAfun(){
@@ -56,8 +83,10 @@ function ZAfun(){
     ZA.style.color="white";
     AZ.style.backgroundColor="black";
     AZ.style.color="white";
-    
+    isAz=false; 
 }
+
+
 
 filterbydate.addEventListener('click',fnDate);
 filterbyletter.addEventListener('click',fnLetter);
@@ -66,6 +95,66 @@ closefilter.addEventListener('click',closefiltercard);
 applybtn.addEventListener('click',apply);
 AZ.addEventListener('click',AZfun);
 ZA.addEventListener('click',ZAfun);
+
+A1.addEventListener('click',A1info);
+A2.addEventListener('click',A2info);
+A3.addEventListener('click',A3info);
+A4.addEventListener('click',A4info);
+B1.addEventListener('click',B1info);
+B2.addEventListener('click',B2info);
+B3.addEventListener('click',B3info);
+B4.addEventListener('click',B4info);
+
+ZA1.addEventListener('click',A1info);
+ZA2.addEventListener('click',A2info);
+ZA3.addEventListener('click',A3info);
+ZA4.addEventListener('click',A4info);
+ZB1.addEventListener('click',B1info);
+ZB2.addEventListener('click',B2info);
+ZB3.addEventListener('click',B3info);
+ZB4.addEventListener('click',B4info);
+
+function A1info(){
+   A1.style.color="red";
+   ZA1.style.color="red";
+}
+function A2info(){
+    A2.style.color="red";
+    ZA2.style.color="red";
+ 
+ }
+ function A3info(){
+    A3.style.color="red";
+    ZA3.style.color="red";
+    
+ }
+ function A4info(){
+    A4.style.color="red";
+    ZA4.style.color="red";
+ }
+ function B1info(){
+    B1.style.color="red";
+    ZB1.style.color="red";
+   
+ }
+ function B2info(){
+     B2.style.color="red";
+     ZB2.style.color="red";
+  }
+  function B3info(){
+     B3.style.color="red";
+     ZB3.style.color="red";
+  }
+  function B4info(){
+     B4.style.color="red";
+     ZB4.style.color="red";
+  }
+
+
+menuicon.addEventListener('click',menubaractive);
+readsection.addEventListener('click',menubaroff);
+window.addEventListener('scroll',menubaroff);
+
 function menubaractive(){
     menucontainer.style.display="flex";
    
@@ -73,8 +162,5 @@ function menubaractive(){
 function menubaroff(){
     menucontainer.style.display="none";
 }
-menuicon.addEventListener('click',menubaractive);
-readsection.addEventListener('click',menubaroff);
-window.addEventListener('scroll',menubaroff);
 
     
